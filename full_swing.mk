20e40a55b901d267dc78aa100e59d38684dc28d1
davidevinavil <davidevinavil@gmail.com>
2013-06-15 14:20:57 +0200

    Clean device tree

--- full_swing.mk
+++ full_swing.mk
@@ -25,12 +25,3 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from mint device
-$(call inherit-product, device/acer/swing/swing.mk)
-
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_swing
-PRODUCT_DEVICE := swing
-PRODUCT_BRAND := Acer
-PRODUCT_MANUFACTURER := Acer
-PRODUCT_MODEL := CloudMobile
