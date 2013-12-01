a23a44d5d8124f3174af2664cdf62b5926ac295c
spacecaker <robin-barry@hotmail.com>
2013-12-01 20:34:50 +0100

    Update device Graphics.(HDPI/XHDPI)

--- device_swing.mk
+++ device_swing.mk
@@ -20,6 +20,7 @@ PRODUCT_COPY_FILES += \
 # if the xhdpi doesn't exist.
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_LOCALES += en_US hdpi
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab \
@@ -75,11 +76,6 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 # Set build date
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-PRODUCT_LOCALES += en_US hdpi
 
 # Audio
 PRODUCT_PACKAGES += \
